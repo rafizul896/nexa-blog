4721998ec827ecbccd1d96e92905d4e5ce2718fa
rafizul896 <rafizulislam896@gmail.com>
2025-03-04 14:12:22 +0600

    add: Retrieve dynamic data with an ID for the detail page using SSR

---
+++ src/app/blogs/[blogId]/page.tsx
@@ -0,0 +1,19 @@
+import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blogData = await res.json();
+
+  return (
+    <div className="my-5">
+      <BlogDetailsCard blog={blogData} />
+    </div>
+  );
+};
+
+export default BlogDetailsPage;
